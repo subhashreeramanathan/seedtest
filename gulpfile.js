06e93c3149695c57189de3baa9f09834e7a2e5cc
subhashreeramanathan <subhashree.ramanathan@syncfusion.com>
2017-10-17 12:18:27 +0530

    gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var Karma = require('karma').Server;
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 var through2 = require('through2');
@@ -26,36 +25,29 @@ var message = {
 
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('src/tsconfig.json', {
-    typescript: require('typescript')
+  typescript: require('typescript')
 });
 gulp.task('start', function () {
-    var tsResult = gulp.src("e2e/**/*.ts")
-        .pipe(tsProject());
+  var tsResult = gulp.src("e2e/**/*.ts")
+    .pipe(tsProject());
 
-    return tsResult.js.pipe(gulp.dest('./e2e'));
+  return tsResult.js.pipe(gulp.dest('./e2e'));
 });
 
-gulp.task('tdd', function (done) {
-    new Karma({
-  
-      configFile: __dirname + '/karma.webpack.conf.js'
-    }, done).start();
-  });
-
-  gulp.task('mail', function (done) {
-    return gulp.src('./e2e/chrome-test-report.html')
-      .pipe(through2.obj(function (file, enc, callback) {
-        var html = file.contents.toString();
-        html = "Hi Everyone,<br><br>Please find the Angular testing report from below. <br>" + html + "<br><br><br>Regards,<br> Web Automation Team";
-        message.html = html;
-        transport.sendMail(message, function (error) {
-          if (error) {
-            console.log('Error occured');
-            console.log(error.message);
-            return;
-          }
-          console.log('Message sent successfully!');
-          process.exit()
-        });
-      }))
-  })
+gulp.task('mail', function (done) {
+  return gulp.src('./e2e/chrome-test-report.html')
+    .pipe(through2.obj(function (file, enc, callback) {
+      var html = file.contents.toString();
+      html = "Hi Everyone,<br><br>Please find the Angular testing report from below. <br>" + html + "<br><br><br>Regards,<br> Web Automation Team";
+      message.html = html;
+      transport.sendMail(message, function (error) {
+        if (error) {
+          console.log('Error occured');
+          console.log(error.message);
+          return;
+        }
+        console.log('Message sent successfully!');
+        process.exit()
+      });
+    }))
+})
