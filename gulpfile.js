f42f5af75e36be3109c8c1222984a04094cf23ca
subhashreeramanathan <subhashree.ramanathan@syncfusion.com>
2017-11-02 11:50:52 +0530

    Content is updated

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,60 @@ var transport = nodemailer.createTransport(smtpTransport({
   tls: { ciphers: 'SSLv3' },
   auth: {
     user: "subhashree.ramanathan@syncfusion.com",
+    pass: "Shree123$"'use strict';
+
+var gulp = require('gulp');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var through2 = require('through2');
+
+var transport = nodemailer.createTransport(smtpTransport({
+  host: 'smtp.office365.com',
+  port: '587',
+  tls: { ciphers: 'SSLv3' },
+  auth: {
+    user: "subhashree.ramanathan@syncfusion.com",
     pass: "Shree123$"
+  }
+}));
+
+var message = {
+  from: 'subhashree.ramanathan@syncfusion.com',
+  to: ' karthickt@syncfusion.com,abinaya.subbiah@syncfusion.com,subhashree.ramanathan@syncfusion.com',
+  subject: 'Angular sample browser testing',
+  html: ""
+};
+
+
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('src/tsconfig.json', {
+  typescript: require('typescript')
+});
+gulp.task('start', function () {
+  var tsResult = gulp.src("e2e/**/*.ts")
+    .pipe(tsProject());
+
+  return tsResult.js.pipe(gulp.dest('./e2e'));
+});
+
+gulp.task('mail', function (done) {
+  return gulp.src('./e2e/chrome-test-report.html')
+    .pipe(through2.obj(function (file, enc, callback) {
+      var html = file.contents.toString();
+      html = "Hi Everyone,<br><br>Please find the Angular(Webpack) e2e testing report from below. <br>" + html + "<br><br><br>Regards,<br> Web Automation Team";
+      message.html = html;
+      transport.sendMail(message, function (error) {
+        if (error) {
+          console.log('Error occured');
+          console.log(error.message);
+          return;
+        }
+        console.log('Message sent successfully!');
+        process.exit()
+      });
+    }))
+})
+
   }
 }));
 
